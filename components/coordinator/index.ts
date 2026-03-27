6f711edd7eb5dc1d9688a3e4708ead4d538c291c
nthaw04 <thaonguyenngoc23122004@gmail.com>
2026-03-27 17:54:14 +0700

    refactor: remove CoordinatorMap, SOSSidebar, ClusterDetailsSheet components
    - Deleted CoordinatorMap.tsx, SOSSidebar.tsx, and ClusterDetailsSheet.tsx files.
    - Removed associated logic and UI elements related to SOS management and mapping.
    - Cleaned up index.ts to reflect the removal of these components.

--- components/coordinator/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CoordinatorMap } from "./CoordinatorMap";
-export { default as SOSSidebar } from "./SOSSidebar";
-export { default as AIDispatchPanel } from "./AIDispatchPanel";
-export { default as ClusterDetailsSheet } from "./ClusterDetailsSheet";
